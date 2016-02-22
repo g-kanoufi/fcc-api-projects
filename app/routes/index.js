70c50952e7fdfd6f42852d96d0f197877855ae5c
Guillaume Kanoufi <g.kanoufi@gmail.com>
2016-02-22 18:00:44 +0000

    Headers API

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,6 +14,7 @@ module.exports = function (app, passport) {
 	}
 
 	var clickHandler = new ClickHandler();
+	
 
 	app.route('/')
 		.get(isLoggedIn, function (req, res) {
@@ -54,4 +55,49 @@ module.exports = function (app, passport) {
 		.get(isLoggedIn, clickHandler.getClicks)
 		.post(isLoggedIn, clickHandler.addClick)
 		.delete(isLoggedIn, clickHandler.resetClicks);
+
+
+	// Added Routes for API projects //
+
+	// Timestamp API project
+	app.route('/timestamp')
+		.get((req, res) => {
+			res.sendFile(path + '/public/timestamp.html');
+	});
+	app.route('/timestamp/:query')
+		.get((req, res) => {
+		var queryDate = req.params.query,
+		    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], 
+		    dateObj = {};
+	
+		if(+queryDate){
+			dateObj.unix = queryDate;
+			var nDate = new Date(queryDate * 1000);
+			dateObj.natural = months[nDate.getMonth()] + ' ' + nDate.getDate() + ', ' + nDate.getFullYear();	
+		}else if(!isNaN(Date.parse(queryDate)) ){
+			dateObj.unix = Date.parse(queryDate) / 1000;
+			dateObj.natural = queryDate;
+		}else{
+			dateObj.unix = null;
+			dateObj.natural = null;
+		}
+			
+		res.end(JSON.stringify(dateObj));
+	});
+	
+	// Headers API Project
+	app.route('/headers')
+		.get( (req, res) => {
+			var headersObj = {},
+			    reg = /(?:\()([\w\s\d;]+)(?:\))/,
+		            userFiltered = reg.exec(req.headers['user-agent']);
+			    
+			
+			headersObj.ip = req.headers['x-forwarded-for'],
+			headersObj.lang = req.headers['accept-language'].split(',')[0],
+			headersObj.software = userFiltered[1];
+			
+			res.end(JSON.stringify(headersObj));
+
+	});
 };
